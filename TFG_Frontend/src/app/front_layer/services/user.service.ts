9e07cc47325e826537143fc5216713ed9878298b
AdrianDelgadoL <adrian.delgadol@e-campus.uab.cat>
2022-04-07 11:56:10 +0200

    Added user service for user credential control

---
+++ TFG_Frontend/src/app/front_layer/services/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {SignOutUseCase} from "../../uc_layer/auth/signout.usecase";
+import {SigninUseCase} from "../../uc_layer/auth/signin.usecase";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private signOutUC: SignOutUseCase,
+              private signinUC: SigninUseCase
+  ) { }
+
+  userId: string = "";
+
+  async signoutUser() {
+    await this.signOutUC.execute(null);
+    this.userId = "";
+  }
+
+  async signinUser(email: string, password: string) {
+    const user = await this.signinUC.execute([email, password]);
+    this.userId = user;
+    return user;
+  }
+}
