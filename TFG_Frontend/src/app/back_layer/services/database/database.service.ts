9f9b387bf9bcd61669dad60ed4849c6764c2cfbc
AdrianDelgadoL <adrian.delgadol@e-campus.uab.cat>
2022-05-20 12:23:48 +0200

    Fixed worker data dissapearing when disabling them

--- TFG_Frontend/src/app/back_layer/services/database/database.service.ts
+++ TFG_Frontend/src/app/back_layer/services/database/database.service.ts
@@ -100,7 +100,7 @@ export class DatabaseService {
 
   // Workers data
   async createWorker(id: string, data: {email: string, speciality: string, name: string, surname: string, available: boolean, role: string}) {
-    await setDoc(doc(this.db, "Personal", id), data);
+    await updateDoc(doc(this.db, "Personal", id), data);
   }
 
   async getWorkers() {
